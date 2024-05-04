0a39e29918fab35dd10f2187a7444ef231ea2823
George Taylor <hey@george.dev>
2024-05-04 15:46:18 +0100

    🐛 Correct action.yml path + github action input vars

--- env.py
+++ env.py
@@ -2,10 +2,10 @@ import os
 
 class Env:
     try:
-        TELEGRAM_BOT_TOKEN=os.environ.get("TELEGRAM_BOT_TOKEN")
+        TELEGRAM_BOT_TOKEN=os.environ.get("INPUT_TELEGRAM_BOT_TOKEN")
     except:
         raise Exception("TELEGRAM_BOT_TOKEN not found in environment variables")
     try:
-        TELEGRAM_CHAT_ID=os.environ.get("TELEGRAM_CHAT_ID")
+        TELEGRAM_CHAT_ID=os.environ.get("INPUT_TELEGRAM_CHAT_ID")
     except:
         raise Exception("TELEGRAM_CHAT_ID not found in environment variables")
