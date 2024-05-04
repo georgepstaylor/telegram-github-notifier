c95b152fb4e3f1a810dad37702b488fbea2625d0
George Taylor <hey@george.dev>
2024-05-04 16:46:11 +0100

    ✨ Use requests library to interact with tg api

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import telegram
 from env import Env
+import requests
+
+telegram_base_url = "https://api.telegram.org/bot"
+
+def telegram_check_token():
+    try:
+        user = requests.get(telegram_base_url + Env.TELEGRAM_BOT_TOKEN + "/getMe")
+        if user.status_code == 200:
+            if user.json()["ok"]:
+                return True
+            else:
+                raise Exception("Authentication failed with Telegram API")
+        else:
+            raise Exception("Request did not return 200 status code")
+    except Exception as e:
+        raise Exception("Request failed to connect to Telegram API with error: " + str(e))
+
+def telegram_send_message(message):
+    requests.post(
+        telegram_base_url + Env.TELEGRAM_BOT_TOKEN + "/sendMessage",
+        json={
+            "chat_id": Env.TELEGRAM_CHAT_ID,
+            "text": message
+        }
+    )
 
 def main():
-    client = telegram.Bot(token=Env.TELEGRAM_BOT_TOKEN)
-    client.send_message(chat_id=Env.TELEGRAM_CHAT_ID, text="Hello, World!")
+    if telegram_check_token():
+        print("Token is valid")
+        telegram_send_message(
+            message="Hello, World!"
+        )
+    else:
+        exit(1)
 
 
 if __name__ == "__main__":
